01d6540bdc41be744cacf6a70ba01baf362e0ba9
kgty0707 <156056438+kgty0707@users.noreply.github.com>
2024-08-13 14:00:41 +0900

    [Gold IV] Title: 미로만들기, Time: 72 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,5 +1,5 @@
-import sys
 from collections import deque
+import sys
 
 input = sys.stdin.readline
 
@@ -14,32 +14,28 @@ for _ in range(N):
 dx = (-1, 1, 0, 0)
 dy = (0, 0, -1, 1)
 
-visited = [[(float('inf'), float('inf')) for _ in range(N)] for _ in range(N)]
+visited = [[-1 for _ in range(N)] for _ in range(N)]
 
 def bfs(x, y):
-    visited[y][x] = (1, 0)
     que = deque([(y, x)])
+    visited[y][x] = 0
 
     while que:
         y, x = que.popleft()
-        current_dist, current_block = visited[y][x]
 
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
 
-            if not (0 <= ny < N and 0 <= nx < N):
-                continue
+            if 0 <= ny < N and 0 <= nx < N:
+                new_block = visited[y][x] + (1 if grid[ny][nx] == 0 else 0)
 
-            new_dist = current_dist + 1
-            new_block = current_block + (1 if grid[ny][nx] == 0 else 0)
-
-            if new_block < visited[ny][nx][1]:
-                visited[ny][nx] = (new_dist, new_block)
-                que.append((ny, nx))
+                if visited[ny][nx] == -1 or new_block < visited[ny][nx]:
+                    visited[ny][nx] = new_block
+                    que.append((ny, nx))  
 
 bfs(0, 0)
 
-min_block = visited[N-1][N-1][1]
+min_block = visited[N-1][N-1]
 
 print(min_block)
