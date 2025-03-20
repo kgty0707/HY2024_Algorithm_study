7c800709da38321cfe410b2f994be8512ef30e17
kgty0707 <156056438+kgty0707@users.noreply.github.com>
2025-03-21 02:10:44 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 44 ms, Memory: 45996 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,34 @@
+import sys
+
+def star(num):
+    if num == 0:
+        return ["*"] 
+
+    prev_pattern = star(num - 1) 
+    size = len(prev_pattern)     
+    new_pattern = []
+
+    for line in prev_pattern:
+        new_pattern.append(line * 3)
+
+    for line in prev_pattern:
+        new_pattern.append(line + " " * size + line)
+
+    for line in prev_pattern:
+        new_pattern.append(line * 3)
+
+    return new_pattern
+
+
+n = int(sys.stdin.readline().strip())
+
+num = 0
+temp = n
+while temp > 1:
+    if temp % 3 != 0:
+        sys.exit()
+    temp //= 3
+    num += 1
+
+result = star(num)
+print("\n".join(result))
