c2ca6a1eb070a3c3531ccbabed556fc24fd70441
kgty0707 <156056438+kgty0707@users.noreply.github.com>
2025-04-04 23:07:12 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,19 @@
+import sys
+
+num = int(sys.stdin.readline().rstrip())
+a = list(map(int, sys.stdin.readline().split()))
+
+a.sort()
+
+def search_for_minimum_time(a, num):
+    min_time = 0
+    wait_time = 0
+    for i in range(num):
+        if wait_time:
+            wait_time = wait_time + a[i]
+        else:
+            wait_time = a[i]
+        min_time += wait_time
+    return min_time
+
+print(search_for_minimum_time(a, num))
