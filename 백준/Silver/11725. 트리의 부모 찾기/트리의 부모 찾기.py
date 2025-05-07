32cf2ec926801008fdd888c3fca47893074e0a02
kgty0707 <156056438+kgty0707@users.noreply.github.com>
2025-05-08 00:29:13 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 2752 ms, Memory: 71720 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+def dfs(node, graph, visited, parent):
+    visited[node] = True
+    for other_node in graph[node]:
+        if not visited[other_node]:
+            parent[other_node] = node
+            dfs(other_node, graph, visited, parent)
+
+
+node = int(input())
+graph = [[] for _ in range(node+1)]
+
+# print(graph)
+
+visited = [False] * (node+1)
+parent = [0] * (node+1)
+
+for _ in range(node-1):
+    node_1, node_2 = map(int, input().split())
+    graph[node_1].append(node_2)
+    graph[node_2].append(node_1)
+
+# print(graph)
+dfs(1, graph, visited, parent)
+
+for i in range(2, node + 1):
+    print(parent[i])
